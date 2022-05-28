bf67f29a44a83ab00d7d198a5ca61dc966fac3a1
Ekonaldi Hutabarat <ekonaldi.hutabarat@mii.co.id>
2022-05-28 16:53:43 +0700

    init project

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native-web'
+
+const Home = () =>{
+    return(
+        <View>
+            <Text>Home Instagramm</Text>
+        </View>
+    )
+}
---
+++ src/pages/Register.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { Text, View } from "react-native-web";
+
+const Register = () =>{
+    return(
+        <View>
+            <Text>Register</Text>
+        </View>
+    )
+}
